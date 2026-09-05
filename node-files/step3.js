c842559225798b768a0fcf2fde13a719f7e767fa
Sal <salvadorchavita02@gmail.com>
2026-09-04 19:41:12 -0700

    Step 3 Done

--- node-files/step3.js
+++ node-files/step3.js
@@ -1,28 +1,54 @@
 const axios = require('axios');
-
 const fs = require('fs');
 
-function cat(path) {
+function cat(path, output = null) {
+    let contents = null;
     try {
-        const contents = fs.readFileSync(path, 'utf8');
-        console.log(contents);
+        contents = fs.readFileSync(path, 'utf8');
+
+        if (output === null) {
+            // '--out' isn't in parameter
+            console.log(contents);
+        } else {
+            // Writes contents to new file
+            fs.writeFileSync(output, contents);
+        }
+
     } catch (err) {
         console.log(err);
     }
 }
 
-function webCat(website) {
+function webCat(website, output = null) {
     axios.get(website).then(response => {
-        console.log(response.data);
+
+        if (output === null) {
+            // '--out' isn't in parameter
+            //console.log("Website output");
+            console.log(response.data);
+        } else {
+            // Writes response HTML to new file
+            fs.writeFileSync(output, response.data);
+        }
     })
     .catch (err => {
         console.log(err);
     })
+    
 }
-const argument = process.argv[2];
 
-if (argument.startsWith('http://') || argument.startsWith('https://')) {
-    webCat(argument);
+const argument = process.argv;
+
+// Ensures correct output to file or console
+if (argument[2] === '--out') {
+    if (argument[4].startsWith('http://') || argument[4].startsWith('https://')) {
+        webCat(argument[4], argument[3]);
+    } else {
+        cat(argument[4], argument[3]);
+    }
+
+} else if (argument[2].startsWith('http://') || argument[2].startsWith('https://')) {
+    webCat(argument[2]);
 } else {
-    cat(argument);
+    cat(argument[2]);
 }
